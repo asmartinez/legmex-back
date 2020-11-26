f3abf09699ac89a010bd1f8cb141b930def7c956
HerLeonidaz <g12ada01@hackademy.mx>
2020-11-26 08:52:05 -0800

    Depuracion

---
+++ src/tpe_col_enc/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Tpe_col_enc
+# Register your models here.
+
+admin.site.register(Tpe_col_enc)
--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from django.contrib.auth.models import User
-
-class Customer(User):
-    pass
-    
-    def get_products(self):
-        pass
