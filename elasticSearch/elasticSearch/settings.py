c16bedadf687151af6e4e40d491e4a6e23995a81
Daniel Torres <danytorresbmx@gmail.com>
2020-12-02 21:19:34 -0600

    conexion con elasticsearch y pruebas

--- elasticSearch/elasticSearch/settings.py
+++ elasticSearch/elasticSearch/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_elasticsearch_dsl', # ElasticSearch
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ELASTICSEARCH_DSL={
+    'default': {
+        'hosts': 'localhost:9200'
+    },
+}
--- elasticSearch/elasticSearch/views.py
+++ elasticSearch/elasticSearch/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from search.models import Car
+from search.documents import CarDocument
 
 
 def bootstrap(request):
     # doc_externo = loader.get_template('principal.html')
     # documento = doc_externo.render()
+    #car = Car(
+    #name="Car two",
+    #color="green",
+    #type=2,
+    #description="A bonito car"
+    #)
+    #car.save()
     return render(request, "principal.html")
 
 def buscar(request):
@@ -16,8 +25,12 @@ def buscar(request):
         if len(producto)>20:
             mensaje="Texto de busqueda demasiado largo"
         else:
+            s = CarDocument.search().filter("term", color=producto)
 
-            return render(request, "resultado_busqueda.html", {"query":producto})
+            for hit in s:
+                print("Car name : {}, description {}".format(hit.name, hit.description))
+
+            return render(request, "resultado_busqueda.html", {"query":s})
         
     else:
         mensaje = "No has introducido un valor"
