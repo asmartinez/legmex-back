59ff13541e064f583ae62094d07513a2e0a74751
Daniel Torres <danytorresbmx@gmail.com>
2020-12-09 20:08:17 -0600

    Conf. para AWS

--- elastic/migrate.sh
+++ elastic/migrate.sh
@@ -2,4 +2,5 @@
 # Scrip para automatizar las migraciones de django y mantener servidor 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic 
+gunicorn elastic.wsgi:application --bind 0.0.0.0:8000
