36e9c61615fab14a8dbc26e119104ae6a06b7d25
HerLeonidaz <g12ada01@hackademy.mx>
2021-01-18 23:03:45 -0800

    vista registro

--- src/registro/views.py
+++ src/registro/views.py
@@ -3,9 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Registro
+from .serializers import RegistroSerializer
 
 # Create your views here.
 class ListRegistro(APIView):
     def get(self, request):
         registros = Registro.objects.all()
-        return Response(registros) 
+        registro_json = RegistroSerializer(registros, many = True)
+        return Response(registro_json.data) 
