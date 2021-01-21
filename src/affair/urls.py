f16d083ff75387255016940a864a3e3b1e7e59da
HerLeonidaz <g12ada01@hackademy.mx>
2021-01-20 16:18:25 -0800

    modifciacion affair list

--- src/affair/urls.py
+++ src/affair/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ListAffair
 
 urlpatterns = [
-    path('affair/',ListTpe_col_cattdisp.as_view(), name='lista-affair' ),
+    path('affair/',ListAffair.as_view(), name='lista-affair' ),
 ]
