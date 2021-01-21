7be91f495c8e5d47ff50c187c07ad410d7dae6ac
Daniel Torres <g12ada02@hackademy.mx>
2021-01-20 19:59:26 -0600

    quitando docker para realizar pruebas

--- elastic/migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Scrip para automatizar las migraciones de django y mantener servidor 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic 
-gunicorn elastic.wsgi:application --bind 0.0.0.0:8000
