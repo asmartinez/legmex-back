89cb1b5810b547573f4ac7753b3bcde0b3108230
Daniel Torres <g12ada02@hackademy.mx>
2021-01-20 23:31:34 -0600

    cambios en la url para descargar archivo

--- elastic/search/urls.py
+++ elastic/search/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('buscar/', views.BuscarDocumento),
     path('subir/', views.SubirDocumento),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
