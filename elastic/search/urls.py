dbf936fe086dfb0b519ef931ff3702b8134ed051
Daniel Torres <g12ada02@hackademy.mx>
2021-02-22 14:00:18 -0600

    se cambia url de search a document y se inserta documento para gitlab

--- elastic/search/urls.py
+++ elastic/search/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.BuscarDocumento), # URL para busqueda de documento
+    path('document/', views.BuscarDocumento), # URL para busqueda de documento
     path('upload/', views.SubirDocumento), # URL para subir un documento 
     path('document/<int:id>/', views.ModificarDocumento.as_view()),
 ]
