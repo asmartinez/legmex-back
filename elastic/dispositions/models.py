f2c9935d2beb52868875e89c7e75d2d75e02dd71
L30 <rzelectronic@gmail.com>
2021-03-02 07:05:35 -0800

    Modificacion modelo disposiciones se agrego campo de clave

--- elastic/dispositions/models.py
+++ elastic/dispositions/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Disposition(models.Model):
     id = models.AutoField(verbose_name='ID', auto_created=True, primary_key=True)
+    clave = models.IntegerField()
     dispositionType = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now=True)
     
