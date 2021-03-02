b5cfcaf752b581300278af7c48bc1b71adeb86e8
Daniel Torres <g12ada02@hackademy.mx>
2021-03-02 16:22:55 -0600

    cambio para no generar conflicto con la DB

--- elastic/dispositions/models.py
+++ elastic/dispositions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Disposition(models.Model):
     id = models.AutoField(verbose_name='ID', auto_created=True, primary_key=True)
-    clave = models.IntegerField()
+    clave = models.IntegerField(blank=True, null=True)
     dispositionType = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now=True)
     
