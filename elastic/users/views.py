e4a2d784d5c11c4af0c82ffe7bf219f4e3d8291b
Daniel Torres <g12ada02@hackademy.mx>
2021-03-08 20:23:06 -0600

    cambio localhost

--- elastic/users/views.py
+++ elastic/users/views.py
@@ -27,7 +27,7 @@ def register(request):
         serializer.save() 
         # Then we get a token for the created user.
         # This could be done differentley 
-        r = requests.post('http://0.0.0.0:8000/o/token/', 
+        r = requests.post('http://localhost:8000/o/token/', 
             data={
                 'grant_type': 'password',
                 'username': request.data['username'],
@@ -49,7 +49,7 @@ def token(request):
     {"username": "username", "password": "1234abcd"}
     '''
     r = requests.post(
-    'http://0.0.0.0:8000/o/token/', 
+    'http://localhost:8000/o/token/', 
         data={
             'grant_type': 'password',
             'username': request.data['username'],
@@ -70,7 +70,7 @@ def refresh_token(request):
     {"refresh_token": "<token>"}
     '''
     r = requests.post(
-    'http://0.0.0.0:8000/o/token/', 
+    'http://localhost:8000/o/token/', 
         data={
             'grant_type': 'refresh_token',
             'refresh_token': request.data['refresh_token'],
@@ -89,7 +89,7 @@ def revoke_token(request):
     {"token": "<token>"}
     '''
     r = requests.post(
-        'http://0.0.0.0:8000/o/revoke_token/', 
+        'http://localhost:8000/o/revoke_token/', 
         data={
             'token': request.data['token'],
             'client_id': CLIENT_ID,
